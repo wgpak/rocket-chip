4c767a80d6cae0f045830669dda23335b0857894
Jonathan Richard Robert Kimmitt <jrrk2@cl.cam.ac.uk>
2019-07-05 11:30:49 +0100

    Reduce chain count for compatibility with Xilinx debug hub

--- src/main/scala/jtag/JtagTap.scala
+++ src/main/scala/jtag/JtagTap.scala
@@ -86,23 +86,20 @@ class JtagTapControllerFPGA(irLength: Int, initialInstruction: BigInt)(implicit 
 
   val io = IO(new JtagControllerIO(irLength))
 
-  val chain02 = Module(new BSCANE2(0x1));
   val chain22 = Module(new BSCANE2(0x3));
   val chain23 = Module(new BSCANE2(0x4));
 
-  chain02.io.TDO <> io.dataChainIn.data
   chain22.io.TDO <> io.dataChainIn.data
   chain23.io.TDO <> io.dataChainIn.data
-  io.dataChainOut.data := (chain02.io.SEL & chain02.io.TDI) | (chain22.io.SEL & chain22.io.TDI) | (chain23.io.SEL & chain23.io.TDI)
+  io.dataChainOut.data := (chain22.io.SEL & chain22.io.TDI) | (chain23.io.SEL & chain23.io.TDI)
   io.jtag.TDO.driven := true.B
-  io.output.instruction := (chain02.io.SEL * 9.asUInt(irLength.W)) +
-                           (chain22.io.SEL * 34.asUInt(irLength.W)) +
+  io.output.instruction := (chain22.io.SEL * 34.asUInt(irLength.W)) +
                            (chain23.io.SEL * 35.asUInt(irLength.W))
-  io.output.reset := chain02.io.RESET | chain22.io.RESET | chain23.io.RESET
-  io.dataChainOut.shift := chain02.io.SHIFT | chain22.io.SHIFT | chain23.io.SHIFT
+  io.output.reset := chain22.io.RESET | chain23.io.RESET
+  io.dataChainOut.shift := chain22.io.SHIFT | chain23.io.SHIFT
   io.jtag.TDO.data := io.dataChainOut.data
-  io.dataChainOut.capture := chain02.io.CAPTURE | chain22.io.CAPTURE | chain23.io.CAPTURE
-  io.dataChainOut.update := chain02.io.UPDATE | chain22.io.UPDATE | chain23.io.UPDATE
+  io.dataChainOut.capture := chain22.io.CAPTURE | chain23.io.CAPTURE
+  io.dataChainOut.update := chain22.io.UPDATE | chain23.io.UPDATE
 }
 
 /** JTAG TAP controller internal block, responsible for instruction decode and data register chain
